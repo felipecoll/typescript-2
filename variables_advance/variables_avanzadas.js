c0dc8979e92df501ae3fc347007ee2ce93d4ecd6
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-31 18:14:30 -0300

    variables avanzadas

---
+++ variables_advance/variables_avanzadas.js
@@ -0,0 +1,10 @@
+var variableAny;
+variableAny = "hello";
+//variableAny = 123
+//variableAny = true
+//console.log("Valor de variable any " + variableAny)
+var variabledDesconocida;
+variabledDesconocida = "hello";
+variabledDesconocida = 123;
+variabledDesconocida = true;
+console.log("Valor de variable unknown " + variabledDesconocida);
---
+++ variables_advance/variables_avanzadas.ts
@@ -0,0 +1,15 @@
+let variableAny: any
+
+variableAny = "hello"
+//variableAny = 123
+//variableAny = true
+
+//console.log("Valor de variable any " + variableAny)
+
+let variabledDesconocida: unknown
+
+variabledDesconocida = "hello"
+variabledDesconocida = 123
+variabledDesconocida = true
+
+console.log("Valor de variable unknown " + variabledDesconocida)
