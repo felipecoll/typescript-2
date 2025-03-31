af632198057bec5208f02d39a7993e832c5ff342
Felipe Coll <leonfelipecoll@gmail.com>
2025-03-31 17:04:48 -0300

    variables base

---
+++ variables_base/variables.js
@@ -0,0 +1,10 @@
+var nombre = "Felipe";
+var edad = 41;
+var esDeveloper = true;
+var mes;
+var varNull = null;
+console.log('Mes de inicio:', mes);
+mes = 'Eneroo';
+console.log('Mes actualizado', mes);
+//console.log('Hola mundo desde Typescript')
+console.log("Hola ".concat(nombre, "!!!"));
---
+++ variables_base/variables.ts
@@ -0,0 +1,14 @@
+let nombre: string = "Felipe"
+let edad: number = 41
+let esDeveloper: boolean = true
+let mes: string | undefined
+let varNull: null = null 
+
+console.log('Mes de inicio:', mes)
+
+mes = 'Eneroo'
+console.log('Mes actualizado', mes)
+
+
+//console.log('Hola mundo desde Typescript')
+console.log(`Hola ${nombre}!!!`)
